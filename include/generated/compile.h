aec1c2aee293136b32d8ae7e8a5108a5c7ab542c
jkbuha <jkbuha@protonmail.com>
2018-07-01 17:55:24 +0100

    Added Mali drivers

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Jul 1 13:42:22 BST 2018"
+#define UTS_VERSION "#5 SMP PREEMPT Sun Jul 1 17:51:01 BST 2018"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
