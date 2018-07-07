641d2877f28f3ed8e471d4e294fdb3db56797159
jkbuha <jkbuha@protonmail.com>
2018-07-07 13:21:16 +0100

    General clean-up

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 33 */
+/* This file is auto generated, version 34 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#33 SMP PREEMPT Thu Jul 5 13:09:33 BST 2018"
+#define UTS_VERSION "#34 SMP PREEMPT Sat Jul 7 13:20:43 BST 2018"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
