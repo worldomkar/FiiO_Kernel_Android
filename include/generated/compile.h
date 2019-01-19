a44e7b7339f508e0978193cd47da01995091df12
jkbuha <jkbuha@protonmail.com>
2019-01-19 14:41:16 +0000

    Fixes to Maple and Zen schedulers

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 65 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#65 SMP PREEMPT Mon Jan 14 17:32:49 GMT 2019"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Jan 19 12:49:18 GMT 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
