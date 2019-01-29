1393c0342e2c4b3ed9642921df512cc380dd5414
jkbuha <jkbuha@protonmail.com>
2019-01-29 16:28:08 +0000

    Enable interactiveX governor

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Jan 27 19:55:51 GMT 2019"
+#define UTS_VERSION "#7 SMP PREEMPT Tue Jan 29 16:27:11 GMT 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
