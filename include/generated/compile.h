fe9c211ec5e4757178429d15e6bf11088b132e5a
jkbuha <jkbuha@protonmail.com>
2019-01-31 13:39:15 +0000

    Enable BFQ and Westwood

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 21 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 SMP PREEMPT Wed Jan 30 21:39:02 GMT 2019"
+#define UTS_VERSION "#21 SMP PREEMPT Thu Jan 31 12:46:08 GMT 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
