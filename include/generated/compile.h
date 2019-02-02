8e5f9a2a42516ff1a613b8b71ca120ae621dc012
jkbuha <jkbuha@protonmail.com>
2019-02-02 17:50:02 +0000

    Additional corrections

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 53 */
+/* This file is auto generated, version 57 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#53 SMP PREEMPT Fri Feb 1 14:42:18 GMT 2019"
+#define UTS_VERSION "#57 SMP PREEMPT Sat Feb 2 15:02:25 GMT 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
