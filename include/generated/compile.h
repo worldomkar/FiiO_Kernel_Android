204520cd758eb9cfbe6a3fb47f05e83277fe423b
jkbuha <jkbuha@protonmail.com>
2019-02-11 13:46:03 +0000

    Added frandom generator

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 59 */
+/* This file is auto generated, version 60 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#59 SMP PREEMPT Mon Feb 11 11:49:34 GMT 2019"
-#define LINUX_COMPILE_BY "root"
+#define UTS_VERSION "#60 SMP PREEMPT Mon Feb 11 11:58:50 GMT 2019"
+#define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
