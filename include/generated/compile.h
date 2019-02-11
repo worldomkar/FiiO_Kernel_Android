56e87dede20f9a750334554e9a0458acbcbfb718
jkbuha <jkbuha@protonmail.com>
2019-02-11 11:52:04 +0000

    Added ROW I/O scheduler

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 57 */
+/* This file is auto generated, version 59 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#57 SMP PREEMPT Sat Feb 2 15:02:25 GMT 2019"
-#define LINUX_COMPILE_BY "jkbuha"
+#define UTS_VERSION "#59 SMP PREEMPT Mon Feb 11 11:49:34 GMT 2019"
+#define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
