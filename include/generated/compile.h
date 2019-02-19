a83d44381e884fc377051554580b9f84d9538994
jkbuha <jkbuha@protonmail.com>
2019-02-19 13:11:09 +0000

    Minor optimisations and fix-ups

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 74 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#71 SMP PREEMPT Tue Feb 19 11:37:13 GMT 2019"
+#define UTS_VERSION "#74 SMP PREEMPT Tue Feb 19 13:09:34 GMT 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
