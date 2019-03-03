d9efae13aac9a9a77c0349edf6e31398c4754e19
jkbuha <jkbuha@protonmail.com>
2019-03-03 17:30:18 +0000

    Missing files

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 91 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#91 SMP PREEMPT Fri Feb 22 22:26:23 GMT 2019"
-#define LINUX_COMPILE_BY "jkbuha"
-#define LINUX_COMPILE_HOST "Virtualbox"
-#define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
