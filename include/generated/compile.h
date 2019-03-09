59d6530fe06a206cf29c62da6422979075a905e1
jkbuha <jkbuha@protonmail.com>
2019-03-09 13:00:54 +0000

    Spring Cleaning

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Mar 9 12:30:14 GMT 2019"
-#define LINUX_COMPILE_BY "jkbuha"
-#define LINUX_COMPILE_HOST "Virtualbox"
-#define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
