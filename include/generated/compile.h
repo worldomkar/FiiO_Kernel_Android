9301cf744df783723f2c711309267813c51eb928
Julian <jkbuha@protonmail.com>
2019-05-23 20:58:23 +0100

    Voltage optimisation for 2016MHz profile

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 31 */
+/* This file is auto generated, version 23 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#31 SMP PREEMPT Sat May 11 22:11:24 BST 2019"
+#define UTS_VERSION "#23 SMP PREEMPT Tue May 21 20:45:57 BST 2019"
 #define LINUX_COMPILE_BY "jkbuha"
-#define LINUX_COMPILE_HOST "prispewnic"
+#define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
