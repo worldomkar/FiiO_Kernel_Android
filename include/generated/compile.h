e8f3a3cfb0dba0788331f7b414cb3addb3b5967e
Julian <jkbuha@protonmail.com>
2019-05-31 11:45:22 +0100

    Initial DSBOOST code

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Thu May 30 14:26:50 BST 2019"
+#define UTS_VERSION "#16 SMP PREEMPT Fri May 31 11:21:24 BST 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
