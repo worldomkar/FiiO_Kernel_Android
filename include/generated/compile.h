14df516a66584e965aca1bdea3bced03c31dddc4
Julian <jkbuha@protonmail.com>
2019-07-23 13:19:34 +0100

    Reverted RCU and SMP code

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sat Jul 20 22:37:20 BST 2019"
+#define UTS_VERSION "#18 SMP PREEMPT Mon Jul 22 21:40:50 BST 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
