ffbba56d01451071e37a7c102297af2d72a974d7
Julian <jkbuha@protonmail.com>
2019-07-23 17:54:32 +0100

    Voltage optimisation for 504MHz

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 20 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 SMP PREEMPT Mon Jul 22 21:40:50 BST 2019"
+#define UTS_VERSION "#20 SMP PREEMPT Tue Jul 23 17:49:25 BST 2019"
 #define LINUX_COMPILE_BY "jkbuha"
 #define LINUX_COMPILE_HOST "Virtualbox"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
