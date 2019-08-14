5bb888c2889d537f23bba69a81e8e1026f6246bf
Julian <jkbuha@protonmail.com>
2019-08-14 21:28:37 +0100

    sched: Skip autogroup when looking for all rt sched groups

--- kernel/sched_autogroup.h
+++ kernel/sched_autogroup.h
@@ -20,6 +20,7 @@ static inline bool task_group_is_autogroup(struct task_group *tg)
 	return 0;
 }
 
+static inline bool task_group_is_autogroup(struct task_group *tg);
 static inline struct task_group *
 autogroup_task_group(struct task_struct *p, struct task_group *tg)
 {
