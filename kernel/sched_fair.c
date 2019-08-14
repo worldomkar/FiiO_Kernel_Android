4211a029b58b5d039cbdbf228b7954cf79e6a8ec
Julian <jkbuha@protonmail.com>
2019-08-14 21:25:04 +0100

    sched: Fix buglet in return_cfs_rq_runtime()

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1760,7 +1760,7 @@ static __always_inline void return_cfs_rq_runtime(struct cfs_rq *cfs_rq)
 	if (!cfs_bandwidth_used())
 		return;
 
-	if (!cfs_rq->runtime_enabled || !cfs_rq->nr_running)
+	if (!cfs_rq->runtime_enabled || cfs_rq->nr_running)
 		return;
 
 	__return_cfs_rq_runtime(cfs_rq);
