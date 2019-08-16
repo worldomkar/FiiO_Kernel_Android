256666333e35ba92e08c0519ea11908cfb3edd28
Julian <jkbuha@protonmail.com>
2019-08-16 18:13:15 +0100

    Removed warnings from kernel-posix-timers

--- kernel/posix-cpu-timers.c
+++ kernel/posix-cpu-timers.c
@@ -288,7 +288,7 @@ void thread_group_cputimer(struct task_struct *tsk, struct task_cputime *times)
 	} else
 		raw_spin_lock_irqsave(&cputimer->lock, flags);
 	*times = cputimer->cputime;
-	spin_unlock_irqrestore(&cputimer->lock, flags);
+	raw_spin_unlock_irqrestore(&cputimer->lock, flags);
 }
 
 /*
@@ -1291,9 +1291,9 @@ static inline int fastpath_timer_check(struct task_struct *tsk)
 	if (sig->cputimer.running) {
 		struct task_cputime group_sample;
 
-		spin_lock(&sig->cputimer.lock);
+		raw_spin_lock(&sig->cputimer.lock);
 		group_sample = sig->cputimer.cputime;
-		spin_unlock(&sig->cputimer.lock);
+		raw_spin_unlock(&sig->cputimer.lock);
 
 		if (task_cputime_expired(&group_sample, &sig->cputime_expires))
 			return 1;
