216d2cf4f5fadb04f1da24aff39b96d55e5be5bf
Julian <jkbuha@protonmail.com>
2019-09-13 13:37:31 +0100

    init, block: try to load default elevator module early during boot

--- init/main.c
+++ init/main.c
@@ -69,6 +69,8 @@
 #include <linux/slab.h>
 #include <linux/perf_event.h>
 #include <linux/random.h>
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -732,6 +734,17 @@ static void __init do_pre_smp_initcalls(void)
 		do_one_initcall(*fn);
 }
 
+/*
+ * This function requests modules which should be loaded by default and is
+ * called twice right after initrd is mounted and right before init is
+ * exec'd.  If such modules are on either initrd or rootfs, they will be
+ * loaded before control is passed to userland.
+ */
+void __init load_default_modules(void)
+{
+	load_default_elevator_module();
+}
+
 static void run_init_process(const char *init_filename)
 {
 	argv_init[0] = init_filename;
@@ -834,6 +847,9 @@ static int __init kernel_init(void * unused)
 	 * we're essentially up and running. Get rid of the
 	 * initmem segments and start the user-mode stuff..
 	 */
+
+	/* rootfs is available now, try loading default modules */
+	load_default_modules();
 
 	init_post();
 	return 0;
